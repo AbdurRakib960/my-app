aac419fc354bbff3cc12331adff347a1d040df42
AbdurRakib960 <rakibabdur960git@gmail.com>
2020-11-20 00:01:44 +0600

    test

---
+++ src/Author.js
@@ -0,0 +1,507 @@
+let author = [
+    {
+        id:0,
+        authorName:"Susana Ruiz",
+        img:"https://randomuser.me/api/portraits/men/69.jpg"
+    },
+    {
+        id:1,
+        authorName:"Neta lehari",
+        img:"https://randomuser.me/api/portraits/men/87.jpg"
+    },
+    {
+        id:2,
+        authorName:"Rakib Talukdar",
+        img:"https://randomuser.me/api/portraits/men/22.jpg"
+    },
+    {
+        id:3,
+        authorName:"Maddison Zeng",
+        img:"https://randomuser.me/api/portraits/men/15.jpg"
+    },
+    {
+        id:4,
+        authorName:"Rahul Ahmed",
+        img:"https://randomuser.me/api/portraits/men/14.jpg"
+    },
+    {
+        id:5,
+        authorName:"Mehmit kumar",
+        img:"https://randomuser.me/api/portraits/men/75.jpg"
+    },
+    {
+        id:6,
+        authorName:"Leah Bishop",
+        img:"https://randomuser.me/api/portraits/women/67.jpg"
+    },
+    {
+        id:7,
+        authorName:"Khadiza Begum",
+        img:"https://randomuser.me/api/portraits/women/12.jpg"
+    },
+    {
+        id:8,
+        authorName:"Rima begum",
+        img:"https://randomuser.me/api/portraits/women/73.jpg"
+    },
+    
+    {
+        id:9,
+        authorName:"Shahin Alom",
+        img:"https://randomuser.me/api/portraits/men/84.jpg"
+    },
+    {
+        id:10,
+        authorName:"Michel Jackson",
+        img:"https://randomuser.me/api/portraits/men/65.jpg"
+    },
+    {
+        id:11,
+        authorName:"Nora Fathehi",
+        img:"https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+        id:12,
+        authorName:"Khulsuma Begum",
+        img:"https://randomuser.me/api/portraits/women/24.jpg"
+    },
+    {
+        id:13,
+        authorName:"Ruksana Jannat",
+        img:"https://randomuser.me/api/portraits/women/39.jpg"
+    },
+    {
+        id:14,
+        authorName:"Sharuk Khan",
+        img:"https://randomuser.me/api/portraits/men/18.jpg"
+    },
+    {
+        id:15,
+        authorName:"Bertt Carlton",
+        img:"https://randomuser.me/api/portraits/men/32.jpg"
+    },
+    {
+        id:16,
+        authorName:"Marcos Luies",
+        img:"https://randomuser.me/api/portraits/men/41.jpg"
+    },
+    {
+        id:17,
+        authorName:"Roy Grey",
+        img:"https://randomuser.me/api/portraits/men/13.jpg"
+    },
+    {
+        id:18,
+        authorName:"Ema Jannat",
+        img:"https://randomuser.me/api/portraits/women/80.jpg"
+    },
+    {
+        id:19,
+        authorName:"Salman Khan",
+        img:"https://randomuser.me/api/portraits/men/26.jpg"
+    },
+    {
+        id:20,
+        authorName:"Shakib Khan",
+        img:"https://randomuser.me/api/portraits/men/28.jpg"
+    },
+    {
+        id:21,
+        authorName:"Lacku Begum",
+        img:"https://randomuser.me/api/portraits/women/26.jpg"
+    },
+    {
+        id:22,
+        authorName:"Mokhbul Ali",
+        img:"https://randomuser.me/api/portraits/men/99.jpg"
+    },
+    {
+        id:23,
+        authorName:"Amit Hasan",
+        img:"https://randomuser.me/api/portraits/men/76.jpg"
+    },
+    {
+        id:24,
+        authorName:"Musfiq Rahman",
+        img:"https://randomuser.me/api/portraits/men/39.jpg"
+    },
+    {
+        id:25,
+        authorName:"Nicolus Grey",
+        img:"https://randomuser.me/api/portraits/men/84.jpg"
+    },
+    {
+        id:26,
+        authorName:"NNusrat Nishi",
+        img:"https://randomuser.me/api/portraits/women/89.jpg"
+    },
+    {
+        id:27,
+        authorName:"Naim Islam",
+        img:"https://randomuser.me/api/portraits/men/6.jpg"
+    },
+    {
+        id:28,
+        authorName:"Elzabet Brown",
+        img:"https://randomuser.me/api/portraits/women/56.jpg"
+    },
+    {
+        id:29,
+        authorName:"Flexi Scot",
+        img:"https://randomuser.me/api/portraits/men/5.jpg"
+    },
+    {
+        id:30,
+        authorName:"Ishak Robinson",
+        img:"https://randomuser.me/api/portraits/men/2.jpg"
+    },
+    {
+        id:31,
+        authorName:"Mahmod Ullah",
+        img:"https://randomuser.me/api/portraits/men/69.jpg"
+    },
+    {
+        id:32,
+        authorName:"Nomi Harish",
+        img:"https://randomuser.me/api/portraits/women/5.jpg"
+    },
+    {
+        id:33,
+        authorName:"Denlish Scoty",
+        img:"https://randomuser.me/api/portraits/men/52.jpg"
+    },
+    {
+        id:34,
+        authorName:"Rohit Sharma",
+        img:"https://randomuser.me/api/portraits/men/75.jpg"
+    },
+    {
+        id:35,
+        authorName:"Alomgir Hasan",
+        img:"https://randomuser.me/api/portraits/men/25.jpg"
+    },
+    {
+        id:36,
+        authorName:"Rebeka Shuva",
+        img:"https://randomuser.me/api/portraits/women/70.jpg"
+    },
+    {
+        id:37,
+        authorName:"Afjal Islam",
+        img:"https://randomuser.me/api/portraits/men/19.jpg"
+    },
+    {
+        id:38,
+        authorName:"Moyna Begum",
+        img:"https://randomuser.me/api/portraits/women/93.jpg"
+    },
+    {
+        id:39,
+        authorName:"Samiya Aktar",
+        img:"https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+        id:40,
+        authorName:"Samiya Aktar",
+        img:"https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+        id:41,
+        authorName:"Shohid Afridy",
+        img:"https://randomuser.me/api/portraits/men/91.jpg"
+    },
+
+     {
+        id:42,
+        authorName:"Lily Begum",
+        img:"https://randomuser.me/api/portraits/women/64.jpg"
+    },
+    {
+        id:43,
+        authorName:"Shusmita Ali",
+        img:"https://randomuser.me/api/portraits/women/72.jpg"
+    },
+    {
+        id:44,
+        authorName:"Summi Akter",
+        img:"https://randomuser.me/api/portraits/women/1.jpg"
+    },
+    {
+        id:45,
+        authorName:"Shahed Akter",
+        img:"https://randomuser.me/api/portraits/men/72.jpg"
+    },
+    {
+        id:46,
+        authorName:"Saymon Islam",
+        img:"https://randomuser.me/api/portraits/men/31.jpg"
+    },
+    {
+        id:47,
+        authorName:"Esat Balsi",
+        img:"https://randomuser.me/api/portraits/men/46.jpg"
+    },
+    {
+        id:48,
+        authorName:"Ashrafu Abir",
+        img:"https://randomuser.me/api/portraits/men/37.jpg"
+    },
+    {
+        id:49,
+        authorName:"Ishita jannat",
+        img:"https://randomuser.me/api/portraits/women/3.jpg"
+    },
+    {
+        id:50,
+        authorName:"Susana Ruiz",
+        img:"https://randomuser.me/api/portraits/women/90.jpg"
+    },
+    {
+        id:51,
+        authorName:"Neta lehari",
+        img:"https://randomuser.me/api/portraits/women/3.jpg"
+    },
+    {
+        id:52,
+        authorName:"Rakib Talukdar",
+        img:"https://randomuser.me/api/portraits/men/49.jpg"
+    },
+    {
+        id:53,
+        authorName:"Maddison Zeng",
+        img:"https://randomuser.me/api/portraits/women/47.jpg"
+    },
+    {
+        id:54,
+        authorName:"Rahul Ahmed",
+        img:"https://randomuser.me/api/portraits/men/14.jpg"
+    },
+    {
+        id:55,
+        authorName:"Mehmit kumar",
+        img:"https://randomuser.me/api/portraits/men/75.jpg"
+    },
+    {
+        id:56,
+        authorName:"Leah Bishop",
+        img:"https://randomuser.me/api/portraits/women/67.jpg"
+    },
+    {
+        id:57,
+        authorName:"Khadiza Begum",
+        img:"https://randomuser.me/api/portraits/women/12.jpg"
+    },
+    {
+        id:58,
+        authorName:"Rima begum",
+        img:"https://randomuser.me/api/portraits/women/73.jpg"
+    },
+    {
+        id:59,
+        authorName:"Ali hasan",
+        img:"https://randomuser.me/api/portraits/men/12.jpg"
+    },
+    {
+        id:60,
+        authorName:"Shahin Alom",
+        img:"https://randomuser.me/api/portraits/men/84.jpg"
+    },
+    {
+        id:61,
+        authorName:"Michel Jackson",
+        img:"https://randomuser.me/api/portraits/men/65.jpg"
+    },
+    {
+        id:62,
+        authorName:"Nora Fathehi",
+        img:"https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+        id:63,
+        authorName:"Khulsuma Begum",
+        img:"https://randomuser.me/api/portraits/women/24.jpg"
+    },
+    {
+        id:64,
+        authorName:"Ruksana Jannat",
+        img:"https://randomuser.me/api/portraits/women/39.jpg"
+    },
+    {
+        id:65,
+        authorName:"Sharuk Khan",
+        img:"https://randomuser.me/api/portraits/men/18.jpg"
+    },
+    {
+        id:66,
+        authorName:"Bertt Carlton",
+        img:"https://randomuser.me/api/portraits/men/32.jpg"
+    },
+    {
+        id:67,
+        authorName:"Marcos Luies",
+        img:"https://randomuser.me/api/portraits/men/41.jpg"
+    },
+    {
+        id:68,
+        authorName:"Roy Grey",
+        img:"https://randomuser.me/api/portraits/men/13.jpg"
+    },
+    {
+        id:69,
+        authorName:"Ema Jannat",
+        img:"https://randomuser.me/api/portraits/women/80.jpg"
+    },
+    {
+        id:70,
+        authorName:"Salman Khan",
+        img:"https://randomuser.me/api/portraits/men/26.jpg"
+    },
+    {
+        id:71,
+        authorName:"Shakib Khan",
+        img:"https://randomuser.me/api/portraits/men/28.jpg"
+    },
+    {
+        id:72,
+        authorName:"Lacku Begum",
+        img:"https://randomuser.me/api/portraits/women/26.jpg"
+    },
+    {
+        id:73,
+        authorName:"Mokhbul Ali",
+        img:"https://randomuser.me/api/portraits/men/99.jpg"
+    },
+    {
+        id:74,
+        authorName:"Amit Hasan",
+        img:"https://randomuser.me/api/portraits/men/76.jpg"
+    },
+    {
+        id:75,
+        authorName:"Musfiq Rahman",
+        img:"https://randomuser.me/api/portraits/men/39.jpg"
+    },
+    {
+        id:76,
+        authorName:"Nicolus Grey",
+        img:"https://randomuser.me/api/portraits/men/84.jpg"
+    },
+    {
+        id:77,
+        authorName:"NNusrat Nishi",
+        img:"https://randomuser.me/api/portraits/women/89.jpg"
+    },
+    {
+        id:78,
+        authorName:"Naim Islam",
+        img:"https://randomuser.me/api/portraits/men/6.jpg"
+    },
+    {
+        id:79,
+        authorName:"Elzabet Brown",
+        img:"https://randomuser.me/api/portraits/women/56.jpg"
+    },
+    {
+        id:80,
+        authorName:"Flexi Scot",
+        img:"https://randomuser.me/api/portraits/men/5.jpg"
+    },
+    {
+        id:81,
+        authorName:"Ishak Robinson",
+        img:"https://randomuser.me/api/portraits/men/2.jpg"
+    },
+    {
+        id:82,
+        authorName:"Mahmod Ullah",
+        img:"https://randomuser.me/api/portraits/men/69.jpg"
+    },
+    {
+        id:83,
+        authorName:"Nomi Harish",
+        img:"https://randomuser.me/api/portraits/women/5.jpg"
+    },
+    {
+        id:84,
+        authorName:"Denlish Scoty",
+        img:"https://randomuser.me/api/portraits/men/52.jpg"
+    },
+    {
+        id:85,
+        authorName:"Rohit Sharma",
+        img:"https://randomuser.me/api/portraits/men/75.jpg"
+    },
+    {
+        id:86,
+        authorName:"Alomgir Hasan",
+        img:"https://randomuser.me/api/portraits/men/25.jpg"
+    },
+    {
+        id:87,
+        authorName:"Rebeka Shuva",
+        img:"https://randomuser.me/api/portraits/women/70.jpg"
+    },
+    {
+        id:88,
+        authorName:"Afjal Islam",
+        img:"https://randomuser.me/api/portraits/men/19.jpg"
+    },
+    {
+        id:89,
+        authorName:"Moyna Begum",
+        img:"https://randomuser.me/api/portraits/women/93.jpg"
+    },
+    {
+        id:90,
+        authorName:"Samiya Aktar",
+        img:"https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+        id:91,
+        authorName:"Samiya Aktar",
+        img:"https://randomuser.me/api/portraits/women/30.jpg"
+    },
+    {
+        id:92,
+        authorName:"Shohid Afridy",
+        img:"https://randomuser.me/api/portraits/men/91.jpg"
+    },
+
+     {
+        id:93,
+        authorName:"Lily Begum",
+        img:"https://randomuser.me/api/portraits/women/64.jpg"
+    },
+    {
+        id:94,
+        authorName:"Shusmita Ali",
+        img:"https://randomuser.me/api/portraits/women/72.jpg"
+    },
+    {
+        id:95,
+        authorName:"Summi Akter",
+        img:"https://randomuser.me/api/portraits/women/1.jpg"
+    },
+    {
+        id:96,
+        authorName:"Shahed Akter",
+        img:"https://randomuser.me/api/portraits/men/72.jpg"
+    },
+    {
+        id:97,
+        authorName:"Saymon Islam",
+        img:"https://randomuser.me/api/portraits/men/31.jpg"
+    },
+    {
+        id:98,
+        authorName:"Esat Balsi",
+        img:"https://randomuser.me/api/portraits/men/46.jpg"
+    },
+    {
+        id:99,
+        authorName:"Ashrafu Abir",
+        img:"https://randomuser.me/api/portraits/men/37.jpg"
+    }
+]
+
+export default author
